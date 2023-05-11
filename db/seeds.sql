12ee5164689d57534444e2141bbd49c76b29049d
codemodeactivate <jeff.zenko@gmail.com>
2023-05-11 09:54:52 -0400

    seeding has begun + prompts

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE employee_tracker_db;
+
+INSERT INTO department (name) VALUES
+    ('Magical Creatures'),
+    ('Magical Law Enforcement'),
+    ('Department of Mysteries'),
+    ('Department of Accidents'),
+    ('Department of Games and Sports'),
+    ('Department of Transportation');
