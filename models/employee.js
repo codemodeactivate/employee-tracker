061280ecda5e9e2025be6552694bccd242f1bdbf
codemodeactivate <jeff.zenko@gmail.com>
2023-05-15 09:20:06 -0400

    further modulization

--- models/employee.js
+++ models/employee.js
@@ -22,3 +22,103 @@ async function viewAllEmployees() {
         await startProgram();
     }
 }
+
+
+
+async function addEmployee() {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "Enter the first name of the employee:",
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "Enter the last name of the employee:",
+            },
+            {
+                type: "input",
+                name: "roleId",
+                message: "Enter the ID of the employee's role:",
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Enter the ID of the employee's manager:",
+            },
+        ]);
+        // Code to add the employee to the database
+        const newEmployee = "INSERT INTO employee SET ?";
+        const result = await db.promise().query(newEmployee, {
+            first_name: answers.firstName,
+            last_name: answers.lastName,
+            role_id: answers.roleId,
+            manager_id: answers.managerId,
+        });
+        console.log(
+            `Added employee ${answers.firstName} ${answers.lastName} with ID ${result.insertId}`
+        );
+        await startProgram();
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+
+async function updateEmployeeRole() {
+    try {
+        const [roles] = await db.promise().query("SELECT id, title FROM role");
+        const roleChoices = roles.map((role) => ({
+          name: role.title,
+          value: role.id,
+        }));
+
+        const [departments] = await db
+          .promise()
+          .query("SELECT id, name FROM department");
+        const departmentChoices = departments.map((department) => ({
+          name: department.name,
+          value: department.id,
+        }));
+
+        const answers = await inquirer.prompt([
+          {
+            type: "list",
+            name: "roleId",
+            message: "Select the role you want to update:",
+            choices: roleChoices,
+          },
+          {
+            type: "input",
+            name: "newRoleTitle",
+            message: "Enter the new title for the role:",
+          },
+          {
+            type: "input",
+            name: "newSalary",
+            message: "Enter the new salary for the role:",
+          },
+          {
+            type: "list",
+            name: "departmentId",
+            message: "Select the department for the role:",
+            choices: departmentChoices,
+          },
+        ]);
+
+        const updateRole = "UPDATE role SET title = ?, salary = ?, department_id = ? WHERE id = ?";
+        await db.promise().query(updateRole, [
+          answers.newRoleTitle,
+          answers.newSalary,
+          answers.departmentId,
+          answers.roleId,
+        ]);
+
+        console.log("Role updated successfully");
+        await startProgram();
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
--- models/role.js
+++ models/role.js
@@ -20,3 +20,52 @@ async function viewAllRoles() {
     }
 }
 
+
+async function addRole() {
+    try {
+      // Fetch the departments from the database
+      const [departments] = await db.promise().query("SELECT id, name FROM department");
+
+      // Create an array of choices for the department prompt
+      const departmentChoices = departments.map((department) => ({
+        name: department.name,
+        value: department.id,
+      }));
+
+      // Ask the user for the role details, including the department
+      const answers = await inquirer.prompt([
+        {
+          type: "input",
+          name: "roleTitle",
+          message: "Enter the title of the role:",
+        },
+        {
+          type: "input",
+          name: "roleSalary",
+          message: "Enter the salary for the role:",
+        },
+        {
+          type: "list",
+          name: "departmentId",
+          message: "Select a department:",
+          choices: departmentChoices,
+        },
+      ]);
+
+      // Insert the new role into the database
+      const newRole = "INSERT INTO role SET ?";
+      const result = await db.promise().query(newRole, {
+        title: answers.roleTitle,
+        salary: answers.roleSalary,
+        department_id: answers.departmentId,
+      });
+
+      console.log(`Added role ${answers.roleTitle} with ID ${result.insertId}`);
+      await startProgram();
+    } catch (error) {
+      console.log("Error:", error);
+      await startProgram();
+    }
+  }
+
+  
