4850f8674b0e223a57433ed150652b0c0f1fe9ed
codemodeactivate <jeff.zenko@gmail.com>
2023-05-15 09:02:37 -0400

    moved add dept

--- models/department.js
+++ models/department.js
@@ -22,3 +22,29 @@ async function viewAllDepartments() {
         await startProgram();
     }
 }
+
+async function addDepartment() {
+    try {
+        const answers = await inquirer.prompt({
+            type: "input",
+            name: "departmentName",
+            message: "Enter the name of the department:",
+        });
+
+        // Create a new department object
+        const department = {
+            name: answers.departmentName,
+        };
+
+        // Insert the new department into the database
+        const newDept = "INSERT INTO department SET ?";
+        const result = await db.promise().query(newDept, department);
+        console.log(
+            `Added department ${department.name} with ID ${result.insertId}`
+        );
+
+        await startProgram();
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
--- utils/inquirer.js
+++ utils/inquirer.js
@@ -70,31 +70,7 @@ async function startProgram() {
 
 
 
-async function addDepartment() {
-    try {
-        const answers = await inquirer.prompt({
-            type: "input",
-            name: "departmentName",
-            message: "Enter the name of the department:",
-        });
 
-        // Create a new department object
-        const department = {
-            name: answers.departmentName,
-        };
-
-        // Insert the new department into the database
-        const newDept = "INSERT INTO department SET ?";
-        const result = await db.promise().query(newDept, department);
-        console.log(
-            `Added department ${department.name} with ID ${result.insertId}`
-        );
-
-        await startProgram();
-    } catch (error) {
-        console.log("Error:", error);
-    }
-}
 
 async function addRole() {
   try {
